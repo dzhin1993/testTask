20d2905f4a75e8d8cb418111969f967b12380bc0
dzhin1993 <zhinkoilya1993@gmail.com>
2020-07-10 20:16:49 +0300

    fix schema.sql

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -3,5 +3,5 @@ DROP TABLE cities IF EXISTS;
 CREATE TABLE cities (
   id        INTEGER IDENTITY PRIMARY KEY,
   city      VARCHAR(255) NOT NULL,
-  message   VARCHAR(255) NOT NULL,
+  message   VARCHAR(255) NOT NULL
 );
