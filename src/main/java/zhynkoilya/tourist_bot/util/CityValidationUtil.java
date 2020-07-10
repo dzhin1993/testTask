7ab9d44989b600d1be0f9cc556568a0c6b6e4817
dzhin1993 <zhinkoilya1993@gmail.com>
2020-07-10 22:29:38 +0300

    fix CityValidationUtil

--- src/main/java/zhynkoilya/tourist_bot/util/CityValidationUtil.java
+++ src/main/java/zhynkoilya/tourist_bot/util/CityValidationUtil.java
@@ -1,46 +1,40 @@
 package zhynkoilya.tourist_bot.util;
 
+import lombok.experimental.UtilityClass;
 import zhynkoilya.tourist_bot.model.City;
+import zhynkoilya.tourist_bot.util.exception.NotFoundException;
 
 import java.util.Optional;
 
+@UtilityClass
 public class CityValidationUtil {
 
-    private CityValidationUtil() {
-    }
-
-    public static <T> T checkFoundOptional(Optional<T> optional, int id) {
+    public City checkFoundOptional(Optional<City> optional, int id) {
         checkNotFoundWithId(optional.isPresent(), id);
         return optional.get();
     }
 
-    public static void checkNotFoundWithId(boolean found, int id) {
+    public void checkNotFoundWithId(boolean found, int id) {
         checkNotFound(found, "id=" + id);
     }
 
-
-    public static <T> T checkNotFound(T object, String msg) {
-        checkNotFound(object != null, msg);
-        return object;
-    }
-
-    public static void checkNotFound(boolean found, String msg) {
+    public void checkNotFound(boolean found, String msg) {
         if (!found) {
             throw new NotFoundException("Not found city with " + msg);
         }
     }
 
-    public static void checkNew(City city) {
+    public void checkNew(City city) {
         if (!city.isNew()) {
             throw new IllegalArgumentException(city + " must be new (id=null)");
         }
     }
 
-    public static void assureIdConsistent(City entity, int id) {
-        if (entity.isNew()) {
-            entity.setId(id);
-        } else if (entity.getId() != id) {
-            throw new IllegalArgumentException(entity + " must be with id=" + id);
+    public void assureIdConsistent(City city, int id) {
+        if (city.isNew()) {
+            city.setId(id);
+        } else if (city.getId() != id) {
+            throw new IllegalArgumentException(city + " must be with id=" + id);
         }
     }
 }
