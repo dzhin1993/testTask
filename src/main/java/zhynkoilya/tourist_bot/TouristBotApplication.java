02f5c672f43dc39ed4e8ece8d171e8f26c777354
dzhin1993 <zhinkoilya1993@gmail.com>
2020-07-10 19:58:48 +0300

    init

---
+++ src/main/java/zhynkoilya/tourist_bot/TouristBotApplication.java
@@ -0,0 +1,13 @@
+package zhynkoilya.tourist_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zhynkoilya/tourist_bot/TouristBotApplicationTests.java
@@ -0,0 +1,13 @@
+package zhynkoilya.tourist_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
